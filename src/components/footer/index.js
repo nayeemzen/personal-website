bdca750987f16a8d0e70405491045a50dce49468
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2017-12-31 23:39:25 -0500

    Fix minor footer warnings

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,16 +4,16 @@ import './footer.css';
 const Footer = () => (
   <div className="footer">
     <a href="https://linkedin.com/in/nayeemzen">
-      <i class="fa fa-linkedin" aria-hidden="true"></i>
+      <i className="fa fa-linkedin" aria-hidden="true"/>
     </a>
     <a href="mailto:nayeem.zen@live.com">
-      <i class="fa fa-envelope-o" aria-hidden="true"></i>
+      <i className="fa fa-envelope-o" aria-hidden="true"/>
     </a>
     <a href="https://git.io/zen">
-      <i class="fa fa-github-alt" aria-hidden="true"></i>
+      <i className="fa fa-github-alt" aria-hidden="true"/>
     </a>
     <a href="https://stackoverflow.com/users/2361055/nayeem-zen">
-      <i class="fa fa-stack-overflow" aria-hidden="true"></i>
+      <i className="fa fa-stack-overflow" aria-hidden="true"/>
     </a>
   </div>
 );
